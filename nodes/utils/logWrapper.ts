e536965869843b8c10fdb71b04c0293652472c6c
Dan Blah <dan@blahs.life>
2025-06-01 00:14:06 -0400

    refactor: Simplify logWrapper to return processor directly, preparing for future logging and error handling enhancements

--- nodes/utils/logWrapper.ts
+++ nodes/utils/logWrapper.ts
@@ -6,15 +6,8 @@ interface Processor {
 }
 
 // Wrapper that provides the interface expected by n8n's vector stores
-export function logWrapper(processor: Processor, context: ISupplyDataFunctions): any {
-	// Return a function that will be called by the vector store
-	return async () => {
-		// Get the input data from the workflow
-		const items = context.getInputData();
-		
-		// Process all items and return documents
-		const documents = await processor.processAll(items);
-		
-		return documents;
-	};
+export function logWrapper<T extends Processor>(processor: T, _context: ISupplyDataFunctions): T {
+	// In the full n8n implementation, this would add logging and error handling
+	// For compatibility we simply return the processor as-is.
+	return processor;
 } 
